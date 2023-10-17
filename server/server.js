dcfd5e62747da5490e7723b7dc1b8c2ae8d390eb
SoeSuNwe <ssnwe@galaxywave.com>
2023-10-17 13:44:52 +0630

    update survey

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.post('/api/lucky-draw-result', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/Survey.js
+++ src/Survey.js
@@ -1,40 +1,94 @@
 import React, { useState } from 'react';
 
+import {ColorRing} from 'react-loader-spinner';
+import axios from 'axios';
+
 function Survey({ onSurveyComplete }) {
-  const [name, setName] = useState('');
-  const [feedback, setFeedback] = useState('');
+    const [formData, setFormData] = useState({
+        name: '',
+        feedback: '',
+    });
+    const [loading, setLoading] = useState(false); // State to manage the loading spinner
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    // Simulate form submission
-    setTimeout(() => {
-      onSurveyComplete();
-    }, 2000);
-  };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-  return (
-    <div>
-      <h1>Survey</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        /><br /><br />
-        <label htmlFor="feedback">Feedback:</label>
-        <textarea
-          id="feedback"
-          value={feedback}
-          onChange={(e) => setFeedback(e.target.value)}
-          required
-        ></textarea><br /><br />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Show the loading spinner
+        setLoading(true);
+
+        try {
+            const response = await axios.post('/api/survey-response', formData);
+
+            // Hide the loading spinner
+            setLoading(false);
+
+            // Handle the response from the server as needed
+            console.log('Server response:', response.data);
+
+            // Reset the form if needed
+            setFormData({
+                name: '',
+                feedback: '',
+            });
+
+            // Notify the parent component that the survey is complete
+            onSurveyComplete();
+        } catch (error) {
+            // Hide the loading spinner on error
+            setLoading(false);
+
+            // Handle any errors that occur during the request
+            console.error('Error submitting survey:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Survey</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </label>
+                <br />
+                <label>
+                    Feedback:
+                    <textarea
+                        name="feedback"
+                        value={formData.feedback}
+                        onChange={handleInputChange}
+                        required
+                    ></textarea>
+                </label>
+                <br />
+                <button type="submit">Submit</button> 
+
+                {/* Display the loader spinner when loading is true */}
+                {loading && (
+                    <ColorRing
+                        type="TailSpin" // Choose the type of spinner you want
+                        color="#00BFFF" // Set the color
+                        height={50} // Set the height
+                        width={50} // Set the width
+                    />
+                )}
+            </form>
+        </div>
+    );
 }
 
 export default Survey;
